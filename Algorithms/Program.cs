7d9b4e03e185c60f75bf331e4f665d397213019b
learn4me2 <47201817+learn4me2@users.noreply.github.com>
2019-02-03 07:55:58 +0000

    Update Program.cs
    forked by user l4m2 and committed to master branch

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -11,6 +11,7 @@ namespace Algorithms
         // test changes
       //SurSamra made changes online.
       //For pull request
+      //forked by l4m2
       Console.WriteLine(AreStringsAnagrams("elvies", "lives"));
       Tuple<int, int> result = FindTwoSum(new[] { 1, 2, 4, 5, 5, 6 }, 10);
       Console.WriteLine(result.Item1 + " " + result.Item2);
